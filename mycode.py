4a029c79b06bc32fd34dda57d8f3cd36eebdbde7
Sarath Babu <sarath.b@atriauniversity.edu.in>
2026-01-22 23:58:53 +0530

    second version of data added

--- mycode.py
+++ mycode.py
@@ -5,11 +5,34 @@ import os
 # Load the Iris dataset
 iris = load_iris()
 
+# Version 1 data
 # Create a DataFrame
 df = pd.DataFrame(
     data=iris.data,
     columns=iris.feature_names
 )
+
+# Version 2 Data
+# sepal ratio
+df["sepal ratio"] = df["sepal length (cm)"]/df["sepal width (cm)"]
+
+cols  = list(df.columns)
+cols.remove("sepal ratio")
+width_index = cols.index("sepal width (cm)")
+cols.insert(width_index+1,"sepal ratio")
+
+df = df[cols]
+
+print(df.head())
+
+# # Version 3
+# # Petal ratio
+# df["petal ratio"] = df["petal length (cm)"]/df["petal width (cm)"]
+
+# cols  = list(df.columns)
+# cols.remove("petal ratio")
+# width_index = cols.index("petal width (cm)")
+# cols.insert(width_index+1,"petal ratio")
 
 # Add target column
 df["target"] = iris.target
